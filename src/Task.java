cd51779e95b9858e1217c5e112f2bf58c6c7ca76
Mikhail Tsiareshchanka <danss22@yandex.by>
2022-06-14 22:06:32 +0300

    Initial commit

---
+++ src/Task.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+    /*
+Написать метод, который принимает список чисел и возвращает минимальное число из списка чисел.
+В main продемонстрировать работу этого метода. Закоммитить, затем запушить эту з-чу в отдельный репозиторий.
+Скинуть ссылку на этот репозиторий в чат.
+ */
+    public class Task {
+        public static void main(String[] args) {
+            List<Integer> numbers = new ArrayList<>();
+            numbers.add(1);
+            numbers.add(5);
+            numbers.add(-87);
+            numbers.add(15);
+            numbers.add(-9);
+            numbers.add(124);
+            numbers.add(67);
+            nums(numbers);
+        }
+
+        static int nums(List<Integer> numbers) {
+            int min = 0;
+            for (Integer number : numbers) {
+                if (number < min) {
+                    min = number;
+                    System.out.println("Минимальное число: " + min);
+                }
+            }
+            return min;
+        }
+    }
+
